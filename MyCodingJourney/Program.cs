722b947928214f67c962581e9f67f913d88aedf9
mehmetseker-dev <mehmet.seker.dev@gmail.com>
2026-01-02 07:58:56 +0300

    Added loops and conditional logic

--- MyCodingJourney/Program.cs
+++ MyCodingJourney/Program.cs
@@ -1,25 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//1.Metinsel veriler (Dosya yolları, isimler)
-string userName = "User";
-string filePath = "C:\\Document\\Report.xlsx";
+//5 farklı EXcel dosyasının adını bir sepete koyuyıruz
+using System.Runtime.CompilerServices;
 
-//2.Sayısal veriler (Maaş, harcama ,döngü sayısı)
-int dailyTaskCount = 5;
-decimal expenseAmount = 1500.75m; // 'm' harfi para birimi (decimal) olduğunu belirtir.
+string[] excelFiles = { "Satislar_Ocak", "Satislar_Subat", "Satislar_Mart", "Giderler_2025", "Personel_Prim", "1113", "1114" };
 
-// 3.Mantıksal veriler (İş bitti mi? Dosya bulundu mu?)
-bool isTaskCompleted = true;
+// Bu sepetin içindeki 1.dosyayı (yazılımda sayma 0'dan başlar) ekrana yazdıralım.
+Console.WriteLine("Lstenin ilk dosyası: " + excelFiles[0]);
 
-if (isTaskCompleted)
+Console.WriteLine("--- Otomasyon Başlıyor ---");
+
+for (int i = 0; i< excelFiles.Length; i++)
 {
-    Console.WriteLine("Harika! Dosya hazır.");
-}
-else
-{
-    Console.WriteLine("Dosya hazırlanma aşamasında");
+    if (excelFiles[i] == "Giderler_2025")
+    {
+        Console.WriteLine("DİKKAT: Bu bütçe dosyasıdır!");
+    }
+    else 
+    {
+    
+    }
+        //i her turda 0, 1 , 2, 3, 4 olacak...
+        Console.WriteLine(excelFiles[i] + ".xlsx dosyası işleniyor...");
+    Console.WriteLine("Veriler kopyalandı ve PDF'e dönüştürüldü.");
+    Console.WriteLine("---------------------------------------");
 }
 
-//Ekrana yazdırma
-Console.WriteLine("Hos geldin " + userName);
-Console.WriteLine("Bugünkü görev sayısı: " + dailyTaskCount);
+Console.WriteLine("Tüm işlemler başarıyla tamamlandı!");
