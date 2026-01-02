441c3d641da2e18b398f5e2bf593707329678df2
mehmetseker-dev <mehmet.seker.dev@gmail.com>
2026-01-02 07:08:25 +0300

    Proje dosyası ekle.

---
+++ MyCodingJourney/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1.Metinsel veriler (Dosya yolları, isimler)
+string userName = "User";
+string filePath = "C:\\Document\\Report.xlsx";
+
+//2.Sayısal veriler (Maaş, harcama ,döngü sayısı)
+int dailyTaskCount = 5;
+decimal expenseAmount = 1500.75m; // 'm' harfi para birimi (decimal) olduğunu belirtir.
+
+// 3.Mantıksal veriler (İş bitti mi? Dosya bulundu mu?)
+bool isTaskCompleted = true;
+
+if (isTaskCompleted)
+{
+    Console.WriteLine("Harika! Dosya hazır.");
+}
+else
+{
+    Console.WriteLine("Dosya hazırlanma aşamasında");
+}
+
+//Ekrana yazdırma
+Console.WriteLine("Hos geldin " + userName);
+Console.WriteLine("Bugünkü görev sayısı: " + dailyTaskCount);
