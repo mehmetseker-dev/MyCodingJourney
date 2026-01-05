b90da0be8c43c37931da88cb7752ccaa0cf123eb
mehmetseker-dev <mehmet.seker.dev@gmail.com>
2026-01-05 23:14:17 +0300

    Added error handling with try-catch blocks

--- MyCodingJourney/Program.cs
+++ MyCodingJourney/Program.cs
@@ -2,23 +2,32 @@
 
 Console.WriteLine("---Budget Control Panel---");
 
-Console.Write("Please Enter Your Monthly Budget: ");
-//Kalvyeden girilen metni(string)okur. (kalvyeden geln herşey stringtir.)
-string? enteredBudget = Console.ReadLine();
-
-//DİKKAT: Klavyeden geşlen herşey 'string' (metin)olarak gelir.
-//Hesap yapabilmek için onu 'decimal' kutusuna dönüştürmemiz (convert) gerekir.
-
-decimal budget = Convert.ToDecimal(enteredBudget);
-
-Console.Write("Enter The Amount Spent: ");
-decimal spending = Convert.ToDecimal(Console.ReadLine());
-
-if(spending > budget)
+try
 {
-    Console.WriteLine("WARNİNG:You exceeded your budget by " + (spending - budget) + " TL");
+    // Hata yapma ihtimali olan kodlar buraya yazılır.
+    Console.Write("Please! Enter a numerical budget; ");
+    decimal budget = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Your budget has been successfully saved: " + budget);
+
+    Console.Write("\nYou've entered your expense.; ");
+    decimal totalExpence = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Your expence has been successfully saved: " + totalExpence);
+
+    decimal budgetGap = budget - totalExpence;
+    Console.WriteLine("\nYour Budget difference:" + budgetGap);
+
+    if (budgetGap >= 0)
+    {
+        Console.WriteLine("\nConguratulations! You did not exceed the budget.");
+    }
+    else
+    {
+        Console.WriteLine("Warning! Your budget has been exceeded.");
+    }
+
 }
-else
+catch (Exception)
 {
-    Console.WriteLine("Congratulations! You are save to " + (budget - spending) + " TL from your budget." );
+    //Eğer yukarıda bir hata olursa, program kapanmaz ve buraya atlar.
+    Console.WriteLine("ERROR: Please enter just numeral! The program crash was prevented.");
 }
